86cf96e48eb5a96f8cfeed68326d9628420dc518
Ratul121 <40421171+Ratul121@users.noreply.github.com>
2024-01-06 12:51:52 +0600

    ok

--- src/script.js
+++ src/script.js
@@ -84,7 +84,7 @@ function getRes(messages, who) {
 
    var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "Bearer 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");
+myHeaders.append("Authorization", "Bearer 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");
 
 
 var raw = JSON.stringify({
